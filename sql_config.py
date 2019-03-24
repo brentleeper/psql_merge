d69392c97b7de459a415bea10ed9eb451aef163d
Brent Leeper <brentleeper@me.com>
2019-03-24 18:21:05 -0500

    Initial Commit

---
+++ sql_config.py
@@ -0,0 +1,21 @@
+db_1 = None
+'''
+db_1 = {
+	"host" : "192.168.1.1", 
+	"user" : "someuser", 
+	"pass" : "mypw",
+	"port" : 5432,
+	"dbase": "mydatabase"
+}
+'''
+
+db_2 = None
+'''
+db_2 = {
+	"host" : "192.168.1.2", 
+	"user" : "someuser2", 
+	"pass" : "mypw2",
+	"port" : 5432,
+	"dbase": "mydatabase2"
+}
+'''
